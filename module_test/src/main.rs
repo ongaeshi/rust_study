59170d847d83d6d71bbb4116755cb68bb0203a94
ongaeshi <ongaeshi0621@gmail.com>
2025-06-16 03:55:02 +0900

    use test

--- module_test/src/main.rs
+++ module_test/src/main.rs
@@ -1,6 +1,10 @@
 mod module_hello;
+use crate::module_hello::module_b;
+use crate::module_hello::module_b::func_b;
 
 fn main() {
     module_hello::print_hello();
     module_hello::module_b::func_b();
+    module_b::func_b();
+    func_b();
 }
