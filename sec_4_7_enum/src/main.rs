95689398cb012292f92c3bbc2423240668fddb79
ongaeshi <ongaeshi0621@gmail.com>
2025-08-24 02:41:41 +0900

    cmp::Ordering を使って実装

--- sec_4_7_enum/src/main.rs
+++ sec_4_7_enum/src/main.rs
@@ -1,16 +1,25 @@
+use std::cmp::Ordering;
+
 enum Sign {
     Positive,
     Zero,
     Negative
 }
 
+// fn determine_sign(x: i32) -> Sign {
+//     if x > 0 {
+//         Sign::Positive
+//     } else if x < 0 {
+//         Sign::Negative
+//     } else {
+//         Sign::Zero
+//     }
+// }
 fn determine_sign(x: i32) -> Sign {
-    if x > 0 {
-        Sign::Positive
-    } else if x < 0 {
-        Sign::Negative
-    } else {
-        Sign::Zero
+    match x.cmp(&0) {
+        Ordering::Greater => Sign::Positive,
+        Ordering::Less => Sign::Negative,
+        Ordering::Equal => Sign::Zero
     }
 }
 
