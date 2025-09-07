e717a78da977d01c10f5b85c39d05f949d392b91
ongaeshi <ongaeshi0621@gmail.com>
2025-09-08 05:16:42 +0900

    sec13 RefCall<T>

---
+++ sec_4_13_refcell/src/main.rs
@@ -0,0 +1,38 @@
+use std::rc::Rc;
+
+struct Node {
+    data: i32,
+    child: Option<Rc<Node>>
+}
+
+fn print_link(start_node: Rc<Node>) {
+    let mut p = start_node;
+    loop {
+        println!("{}", p.data);
+        if p.child.is_none() {
+            break;
+        }
+        p = Rc::clone(p.child.as_ref().unwrap());
+    }
+}
+
+fn main() {
+    let node3 = Rc::new(Node {
+        data: 3,
+        child: None,
+    });
+    let node1 = Rc::new(Node {
+        data: 1,
+        child: Some(Rc::clone(&node3)),
+    });
+    let node2 = Rc::new(Node {
+        data: 2,
+        child: Some(Rc::clone(&node3)),
+    });
+
+    println!("link from node1");
+    print_link(node1);
+
+    println!("link from node2");
+    print_link(node2);
+}
