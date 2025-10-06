e5dd32318b258f3d27e1594e14c5a6b769b91c51
ongaeshi <ongaeshi0621@gmail.com>
2025-10-07 00:24:56 +0900

    Arc 単体だと mut は取得できない

---
+++ sec_8_2_write_threads/src/main.rs
@@ -0,0 +1,23 @@
+use std::sync::Arc;
+use std::thread::spawn;
+
+fn main() {
+    let data = Arc::new(Vec::new());
+
+    let added = vec![1, 2, 3, 4, 5];
+
+    let mut thrd = Vec::new();
+    for aa in added {
+        let data = Arc::clone(&data);
+        let th = spawn(move || {
+            data.push(aa);
+        });
+        thrd.push(th);
+    }
+
+    thrd.into_iter().for_each(|th| {
+        let _ = th.join();
+    });
+
+    println!("{:?}", *data);
+}
